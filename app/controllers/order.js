e8cccc9a115cbadb8f14245e35f8c37390f1c3df
bestyxie <bestyxiejiayun@gmail.com>
2017-01-30 18:30:08 +0800

    count and sum

--- app/controllers/order.js
+++ app/controllers/order.js
@@ -20,24 +20,32 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 var create_order = exports.create_order = function create_order(req, res) {
   var prod_msg = req.body.products;
   var user_id = prod_msg.user_id;
+  var count = 0,
+      sum = 0;
 
   _ShoppingCart2.default.findOne({ userId: user_id }, { products: 1 }, function (err, prodts) {
     var _prodts = [],
         _prod_msg = [];
+
     if (Array.isArray(prod_msg._id)) {
       _prod_msg = (0, _unique.toString)(prod_msg._id);
     } else {
       _prod_msg.push(prod_msg._id.toString());
     }
+
     for (var i = 0, len = prodts.products.length; i < len; i++) {
-      console.log(prodts.products[i].productId.toString());
       if (_prod_msg.indexOf(prodts.products[i].productId.toString()) > -1) {
         _prodts.push(prodts.products[i]);
+        count += prodts.products[i].qty;
+        sum += prodts.products[i].qty * prodts.products[i].qty;
       }
     }
+
     res.render('mobile/order/create_order', {
       products: _prodts,
-      user_id: user_id
+      user_id: user_id,
+      count: count,
+      sum: sum
     });
   });
 };
--- dev/app/controllers/order.js
+++ dev/app/controllers/order.js
@@ -5,24 +5,31 @@ import { toString } from '../common/unique';
 export const create_order = (req,res) => {
   let prod_msg = req.body.products;
   let user_id = prod_msg.user_id;
+  let count = 0, sum = 0;
 
   ShoppingCart.findOne({userId: user_id},{products: 1},(err,prodts) => {
     let _prodts = [], _prod_msg = [];
+    
     if ( Array.isArray(prod_msg._id) ){
       _prod_msg = toString(prod_msg._id);
     }
     else {
       _prod_msg.push(prod_msg._id.toString());
     }
+
     for(let i = 0,len = prodts.products.length;i<len;i++) {
-      console.log( prodts.products[i].productId.toString());
       if( _prod_msg.indexOf(prodts.products[i].productId.toString()) > -1 ) {
         _prodts.push(prodts.products[i]);
+        count += prodts.products[i].qty;
+        sum += prodts.products[i].qty*prodts.products[i].qty;
       }
     }
+
     res.render('mobile/order/create_order',{
       products: _prodts,
-      user_id: user_id
+      user_id: user_id,
+      count: count,
+      sum: sum
     });
   })
 }
