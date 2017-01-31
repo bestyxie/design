90f17cb6d3432dcf8dd4091bc7b58132eceb7bf4
bestyxie <bestyxiejiayun@gmail.com>
2017-01-31 19:16:55 +0800

    address list style update

--- app/controllers/address.js
+++ app/controllers/address.js
@@ -4,4 +4,20 @@ var User = require('../models/user');
 
 module.exports.add = function (req, res) {
   var addr = req.body.address;
+  var userid = req.body.userid;
+
+  User.findOne({ _id: userid }, function (err, user) {
+    if (err) {
+      console.log(err);
+      res.json({ success: false });
+    }
+    user.address = addr;
+    user.save(function (err) {
+      if (err) {
+        console.log(err);
+        res.json({ success: false });
+      }
+      res.json({ success: true });
+    });
+  });
 };
--- dev/app/controllers/address.js
+++ dev/app/controllers/address.js
@@ -2,4 +2,20 @@ let User = require('../models/user');
 
 module.exports.add = (req,res) => {
   let addr = req.body.address;
+  let userid = req.body.userid;
+
+  User.findOne({_id: userid},(err,user) => {
+    if(err){
+      console.log(err);
+      res.json({success: false});
+    }
+    user.address = addr;
+    user.save(err => {
+      if(err) {
+        console.log(err);
+        res.json({success: false});
+      }
+      res.json({success: true});
+    });
+  })
 }
