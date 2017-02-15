bf8ade3229e19f26457b6906ee3c5e6a61a43db1
bestyxie <bestyxiejiayun@gmail.com>
2017-02-15 15:53:13 +0800

    fixed bug

--- dev/app/controllers/service.js
+++ dev/app/controllers/service.js
@@ -17,19 +17,32 @@ export const _new = (req,res) => {
       console.log(err);
       return;
     }
-    
-    // if((new Date()) - token.create_at < 1000*60*60*2){
-    //   new_server(token.access_token);
-    // }else{
+    if(token && token.length>0){
+
+      // if((new Date()) - token.create_at < 1000*60*60*2){
+      //   new_server(token.access_token);
+      // }else{
+        request.get('https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid='+config.wx.app_id+'&secret='+config.wx.app_secret,function(err,response,body){
+          token.create_at = new Date();
+          let _body = JSON.parse(body);
+          let access_token = _body.access_token;
+          token.access_token = access_token;
+          token.save();
+          new_server(access_token);
+        })
+      // }
+    }else{
       request.get('https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid='+config.wx.app_id+'&secret='+config.wx.app_secret,function(err,response,body){
-        token.create_at = new Date();
+        let _token = {}
+        _token.create_at = new Date();
         let _body = JSON.parse(body);
+        console.log(_body);
         let access_token = _body.access_token;
-        token.access_token = access_token;
-        token.save();
+        _token.access_token = access_token;
+        _token.save();
         new_server(access_token);
       })
-    // }
+    }
 
     function new_server(access_token){
       request.post('https://api.weixin.qq.com/customservice/kfaccount/add?access_token='+token.access_token,server,function(result){
